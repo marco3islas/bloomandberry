7bf1266c24ba9652c59124d8bf518cc7234717f0
marco3islas <marco3islas@gmail.com>
2025-09-20 07:19:07 -0600

    Agregados nuevos productos

---
+++ src/app/conservas/interfaces/tes-interface.ts
@@ -0,0 +1,57 @@
+export interface Tes {
+  sabor: string;
+  ingredientes: string;
+  beneficios: string;
+  presentacion: string;
+}
+
+export const TES_CATEGORIES: Tes[] = [
+  {
+    sabor: 'Jamaica & Frutos Rojos',
+    ingredientes: 'Jamaica, arándano, frambuesa deshidratada',
+    beneficios: 'Antioxidante, refrescante',
+    presentacion: 'Frasco 100 g / bolsitas individuales'
+  },
+  {
+    sabor: 'Tamarindo & Ciruela Pasa Detox',
+    ingredientes: 'Tamarindo, ciruela pasa, jengibre',
+    beneficios: 'Digestivo, regulador intestinal',
+    presentacion: 'Frasco 120 g (infusión granel)'
+  },
+  {
+    sabor: 'Menta & Piña Digestivo',
+    ingredientes: 'Menta, piña deshidratada',
+    beneficios: 'Digestivo, refrescante',
+    presentacion: 'Bolsitas individuales (10-20 piezas)'
+  },
+  {
+    sabor: 'Anís & Frutas Locales',
+    ingredientes: 'Anís estrella, manzana seca, jamaica',
+    beneficios: 'Relajante, ayuda a la digestión',
+    presentacion: 'Frasco 80 g / bolsitas'
+  },
+  {
+    sabor: 'Cacao & Canela (Afrodisíaco)',
+    ingredientes: 'Cáscara de cacao, canela, chile ancho seco',
+    beneficios: 'Estimulante, energizante',
+    presentacion: 'Frasco 100 g gourmet'
+  },
+  {
+    sabor: 'Damiana & Jamaica Afrodita',
+    ingredientes: 'Damiana, jamaica, pétalos de rosa',
+    beneficios: 'Afrodisíaco suave, relajante',
+    presentacion: 'Frasco 80 g con mezcla visible'
+  },
+  {
+    sabor: 'Jengibre & Miel Vital',
+    ingredientes: 'Jengibre, miel cristalizada, limón deshidratado',
+    beneficios: 'Activa circulación, energizante',
+    presentacion: 'Bolsitas individuales'
+  },
+  {
+    sabor: 'Maca & Frutos Secos Premium',
+    ingredientes: 'Maca en polvo, arándanos, dátiles',
+    beneficios: 'Energético, afrodisíaco, balance',
+    presentacion: 'Frasco de vidrio 120 g (edición premium)'
+  }
+]
--- src/app/conservas/pages/productos/productos.ts
+++ src/app/conservas/pages/productos/productos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { PRODUCT_CATEGORIES, PRODUCT_PACKS, PRODUCT_PRESENTATIONS } from '../../interfaces/productos-interface';
 import { CommonModule } from '@angular/common';
+import { TES_CATEGORIES } from '../../interfaces/tes-interface';
 
 @Component({
   selector: 'app-productos',
@@ -14,5 +15,6 @@ export default class Productos {
   productCategories = PRODUCT_CATEGORIES;
   productPresentations = PRODUCT_PRESENTATIONS;
   productPacks = PRODUCT_PACKS;
+  tesCategories = TES_CATEGORIES;
 
 }
